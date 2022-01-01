17af7f4f38a4728e3e58f628e8fe046980a76102
foningivan1 <60967384+foningivan1@users.noreply.github.com>
2022-01-01 21:50:14 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,68 @@
+'<?php 
+  $nom = "junior Essono";
+  $architecture = "Architecte logiciel / DevOps";
+?>
+
+
+
+
+<div class="profile-text"> 
+              
+                <div class="row" style="position: relative;display: flex;">
+                    <!-- <div class="imgBx">
+                        <img src="img/i.jpg" alt="Rakib"> 
+                    </div> -->
+                    <div style="grid-template-rows: 1fr 1fr;">
+                        <div class="search">
+                            <div >
+                                <img src="img/icons8_menu_50px_1.png"  class="menu" alt="">
+                            </div>
+                            <div >
+                               <span style="color:#B9B9B9;">  <p style="margin-top: -6px;margin-left: 25px;">besoin d'un chef de projet  </p> </span> 
+                               
+                            </div>
+                            <div style="width: 35px;">
+                                <img src="img/icons8_search_50px.png" style="width: 25px;height: 25px;margin-left: 60px;margin-top: 15px;"  alt="">
+                                
+                            </div>
+                            <div style="width: 5px;">
+                               <p style="margin-left: 70px; margin-top: 15px;color: #b9b9b9;width: 5px;height: 30px;">
+                                   |
+                               </p>
+                            </div>
+                            <div  style="width: 50px;">
+                                <img src="img/icons8_multiply_50px.png" style="width: 35px;height: 35px;margin-top: 9px;margin-left: 80px;" alt="">
+                            </div>
+                        </div>
+                        <div style="margin-top: 60px;"></div>
+                        <div class="round">
+                             <div class="rounded">
+
+                                <?php echo '<img src="img/esss.PNG"  style="border-radius: 90px;margin-top: 120px;margin-left: 20px;"  alt="">' ?>
+                            
+                            </div>
+                              <div class="desc" style="margin-top: 160px;">
+                                  <span style="color: white;font-size: 32px;"><STRong> <?php echo $nom ?>  </STRong></span><br>
+                                  <span>
+                                      <?php echo $architecture?>
+                                    </span>
+                              </div>
+                              
+                        </div>
+                        <div  style="background-color: #e34856;width: 70px;height: 70px;margin-left: 80%;border-radius: 90px;margin-top: -15px;">
+                            <span style="font-size: 17px;"><p style="color: white;margin-left: 23px;margin-top: -6px;font-size: 40px;">+</p></span> 
+                            
+                        </div>
+                        <div  type="button"  style="background-color: blue;width: 55px;height: 55px;margin-left: 65%;border-radius: 90px;margin-top: -60px;" onClick="alert('envoyé');" >
+                             <img src="img/icons8_email_send_30px.png" style="margin-top: 15px; margin-left:12px" alt=""> 
+                            
+                        </div>
+                    </div> 
+                    
+                
+                         
+                </div> 
+                
+               
+            </div>'
+           
